eadf2e944a32e7f6707b4590db9e6601a3afb74d
purisantosh <116016459+purisantosh@users.noreply.github.com>
2026-03-08 01:47:18 -0500

    remove unused imports

--- src/main/java/com/example/spring_security_6/controller/WelcomeController.java
+++ src/main/java/com/example/spring_security_6/controller/WelcomeController.java
@@ -1,8 +1,6 @@
 package com.example.spring_security_6.controller;
 
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
