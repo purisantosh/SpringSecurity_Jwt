c80a8d140f81851c8ec0ae213b475357282b7fec
purisantosh <116016459+purisantosh@users.noreply.github.com>
2026-03-08 16:20:59 -0400

    added user details service (added multiple users)

--- src/main/java/com/example/spring_security_6/config/WebSecurityConfig.java
+++ src/main/java/com/example/spring_security_6/config/WebSecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +25,20 @@ public class WebSecurityConfig {
         ).formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails santosh= User.withUsername("santosh")
+                .password("{noop}password")
+                .roles("USER").build();
+
+        UserDetails saiNath= User.withUsername("sainath")
+                .password("{noop}sainath")
+                .roles("USER").build();
+
+        return new InMemoryUserDetailsManager(santosh,saiNath);
+
     }
 }
